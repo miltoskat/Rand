0735fc942e91724c77d32f9515ed27c2584e67e8
miltoskat <miltoskat@hotmail.com>
2016-06-30 14:46:01 +0300

    2nd big commit
    The application now produces 20digit random numbers, instead of 15 that
    did on the first commit, also every single number is individualised
    inside the program it is not the best way, but i was too bored to
    convert arc4random_uniform in order to be able to use Int64 numbers
    (for now it only accepts as big as Int32)

--- RandomNumber/RandomNumber/ViewController.swift
+++ RandomNumber/RandomNumber/ViewController.swift
@@ -11,6 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var screenLabel: UILabel!
+    @IBOutlet weak var number0: UILabel!
+    @IBOutlet weak var number1: UILabel!
+    @IBOutlet weak var number2: UILabel!
+    @IBOutlet weak var number3: UILabel!
+    @IBOutlet weak var number4: UILabel!
+    @IBOutlet weak var number5: UILabel!
+    @IBOutlet weak var number6: UILabel!
+    @IBOutlet weak var number7: UILabel!
+    @IBOutlet weak var number8: UILabel!
+    @IBOutlet weak var number9: UILabel!
+    @IBOutlet weak var number10: UILabel!
+    @IBOutlet weak var number11: UILabel!
+    @IBOutlet weak var number12: UILabel!
+    @IBOutlet weak var number13: UILabel!
+    @IBOutlet weak var number14: UILabel!
+    @IBOutlet weak var number15: UILabel!
+    @IBOutlet weak var number16: UILabel!
+    @IBOutlet weak var number17: UILabel!
+    @IBOutlet weak var number18: UILabel!
+    @IBOutlet weak var number19: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     var myTimer : NSTimer = NSTimer()
@@ -39,11 +59,32 @@ class ViewController: UIViewController {
     
     
     func randomizer(){
-        screenLabel.text = ("\(String(arc4random_uniform(89999999) + 10000000))\(String(arc4random_uniform(8999999) + 1000000))")
-        //10.000.000
+        number0.text = ("\(arc4random_uniform(10))")
+        number1.text = ("\(arc4random_uniform(10))")
+        number2.text = ("\(arc4random_uniform(10))")
+        number3.text = ("\(arc4random_uniform(10))")
+        number4.text = ("\(arc4random_uniform(10))")
+        number5.text = ("\(arc4random_uniform(10))")
+        number6.text = ("\(arc4random_uniform(10))")
+        number7.text = ("\(arc4random_uniform(10))")
+        number8.text = ("\(arc4random_uniform(10))")
+        number9.text = ("\(arc4random_uniform(10))")
+        number10.text = ("\(arc4random_uniform(10))")
+        number11.text = ("\(arc4random_uniform(10))")
+        number12.text = ("\(arc4random_uniform(10))")
+        number13.text = ("\(arc4random_uniform(10))")
+        number14.text = ("\(arc4random_uniform(10))")
+        number15.text = ("\(arc4random_uniform(10))")
+        number16.text = ("\(arc4random_uniform(10))")
+        number17.text = ("\(arc4random_uniform(10))")
+        number18.text = ("\(arc4random_uniform(10))")
+        number19.text = ("\(arc4random_uniform(10))")
+
+    
     }
     
     func start(){
+        myTimer.invalidate()
         myTimer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: #selector(ViewController.randomizer), userInfo: nil, repeats: true)
     }
     
