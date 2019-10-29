4357aa219cb99279e3f0288bc4b5f82533b840c3
Vasilis <billsakkas@me.com>
2019-10-29 12:39:44 +0200

    Updated for iOS 13.1 Bug fixes.

--- RandomNumber/RandomNumber/ViewController.swift
+++ RandomNumber/RandomNumber/ViewController.swift
@@ -33,26 +33,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var number19: UILabel!
     @IBOutlet weak var startButton:UIButton!
     @IBOutlet weak var stopButton: UIButton!
-    var myTimer0 : NSTimer = NSTimer()
-    var myTimer1 : NSTimer = NSTimer()
-    var myTimer2 : NSTimer = NSTimer()
-    var myTimer3 : NSTimer = NSTimer()
-    var myTimer4 : NSTimer = NSTimer()
-    var myTimer5 : NSTimer = NSTimer()
-    var myTimer6 : NSTimer = NSTimer()
-    var myTimer7 : NSTimer = NSTimer()
-    var myTimer8 : NSTimer = NSTimer()
-    var myTimer9 : NSTimer = NSTimer()
-    var myTimer10 : NSTimer = NSTimer()
-    var myTimer11 : NSTimer = NSTimer()
-    var myTimer12 : NSTimer = NSTimer()
-    var myTimer13 : NSTimer = NSTimer()
-    var myTimer14 : NSTimer = NSTimer()
-    var myTimer15 : NSTimer = NSTimer()
-    var myTimer16 : NSTimer = NSTimer()
-    var myTimer17 : NSTimer = NSTimer()
-    var myTimer18 : NSTimer = NSTimer()
-    var myTimer19 : NSTimer = NSTimer()
+    var myTimer0 : Timer = Timer()
+    var myTimer1 : Timer = Timer()
+    var myTimer2 : Timer = Timer()
+    var myTimer3 : Timer = Timer()
+    var myTimer4 : Timer = Timer()
+    var myTimer5 : Timer = Timer()
+    var myTimer6 : Timer = Timer()
+    var myTimer7 : Timer = Timer()
+    var myTimer8 : Timer = Timer()
+    var myTimer9 : Timer = Timer()
+    var myTimer10 : Timer = Timer()
+    var myTimer11 : Timer = Timer()
+    var myTimer12 : Timer = Timer()
+    var myTimer13 : Timer = Timer()
+    var myTimer14 : Timer = Timer()
+    var myTimer15 : Timer = Timer()
+    var myTimer16 : Timer = Timer()
+    var myTimer17 : Timer = Timer()
+    var myTimer18 : Timer = Timer()
+    var myTimer19 : Timer = Timer()
     var repeater:Double = 0.0
     
     override func viewDidLoad() {
@@ -61,10 +61,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startButton(sender: AnyObject) {
+        print("Start BTN Pressed")
         start()
     }
     
     @IBAction func stopButton(sender: AnyObject) {
+        print("Stop BTN Pressed")
         stop()
     }
     
@@ -73,65 +75,65 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func randomizer0(){
+    @objc func randomizer0(){
         number0.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer1(){
+    @objc func randomizer1(){
         number1.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer2(){
+    @objc func randomizer2(){
         number2.text = ("\(arc4random_uniform(10))")
     }
 
-    func randomizer3(){
+    @objc func randomizer3(){
         number3.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer4(){
+    @objc func randomizer4(){
         number4.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer5(){
+    @objc func randomizer5(){
         number5.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer6(){
+    @objc func randomizer6(){
         number6.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer7(){
+    @objc func randomizer7(){
         number7.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer8(){
+    @objc func randomizer8(){
         number8.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer9(){
+    @objc func randomizer9(){
         number9.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer10(){
+    @objc func randomizer10(){
         number10.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer11(){
+    @objc func randomizer11(){
         number11.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer12(){
+    @objc func randomizer12(){
         number12.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer13(){
+    @objc func randomizer13(){
         number13.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer14(){
+    @objc func randomizer14(){
         number14.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer15(){
+    @objc func randomizer15(){
         number15.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer16(){
+    @objc func randomizer16(){
         number16.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer17(){
+    @objc func randomizer17(){
         number17.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer18(){
+    @objc func randomizer18(){
         number18.text = ("\(arc4random_uniform(10))")
     }
-    func randomizer19(){
+    @objc func randomizer19(){
         number19.text = ("\(arc4random_uniform(10))")
     }
     func randRandomizer(){
@@ -159,26 +161,26 @@ class ViewController: UIViewController {
         myTimer17.invalidate()
         myTimer18.invalidate()
         myTimer19.invalidate()
-        myTimer0 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer0), userInfo: nil, repeats: true)
-        myTimer1 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer1), userInfo: nil, repeats: true)
-        myTimer2 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer2), userInfo: nil, repeats: true)
-        myTimer3 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer3), userInfo: nil, repeats: true)
-        myTimer4 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer4), userInfo: nil, repeats: true)
-        myTimer5 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer5), userInfo: nil, repeats: true)
-        myTimer6 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer6), userInfo: nil, repeats: true)
-        myTimer7 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer7), userInfo: nil, repeats: true)
-        myTimer8 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer8), userInfo: nil, repeats: true)
-        myTimer9 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer9), userInfo: nil, repeats: true)
-        myTimer10 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer10), userInfo: nil, repeats: true)
-        myTimer11 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer11), userInfo: nil, repeats: true)
-        myTimer12 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer12), userInfo: nil, repeats: true)
-        myTimer13 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer13), userInfo: nil, repeats: true)
-        myTimer14 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer14), userInfo: nil, repeats: true)
-        myTimer15 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer15), userInfo: nil, repeats: true)
-        myTimer16 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer16), userInfo: nil, repeats: true)
-        myTimer17 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer17), userInfo: nil, repeats: true)
-        myTimer18 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer18), userInfo: nil, repeats: true)
-        myTimer19 = NSTimer.scheduledTimerWithTimeInterval(repeater, target: self, selector: #selector(ViewController.randomizer19), userInfo: nil, repeats: true)
+        myTimer0 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(randomizer0), userInfo: nil, repeats: true)
+        myTimer1 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer1), userInfo: nil, repeats: true)
+        myTimer2 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer2), userInfo: nil, repeats: true)
+        myTimer3 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer3), userInfo: nil, repeats: true)
+        myTimer4 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer4), userInfo: nil, repeats: true)
+        myTimer5 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer5), userInfo: nil, repeats: true)
+        myTimer6 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer6), userInfo: nil, repeats: true)
+        myTimer7 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer7), userInfo: nil, repeats: true)
+        myTimer8 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer8), userInfo: nil, repeats: true)
+        myTimer9 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer9), userInfo: nil, repeats: true)
+        myTimer10 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer10), userInfo: nil, repeats: true)
+        myTimer11 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer11), userInfo: nil, repeats: true)
+        myTimer12 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer12), userInfo: nil, repeats: true)
+        myTimer13 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer13), userInfo: nil, repeats: true)
+        myTimer14 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer14), userInfo: nil, repeats: true)
+        myTimer15 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer15), userInfo: nil, repeats: true)
+        myTimer16 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer16), userInfo: nil, repeats: true)
+        myTimer17 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer17), userInfo: nil, repeats: true)
+        myTimer18 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer18), userInfo: nil, repeats: true)
+        myTimer19 = Timer.scheduledTimer(timeInterval: repeater, target: self, selector: #selector(ViewController.randomizer19), userInfo: nil, repeats: true)
     }
     
     func stop(){
